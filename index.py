c49e65b386263c724a7f441d6834ab9b1fcae4c6
Victoria Vilas Boas <victoriaboas@MacBook-Air-de-Victoria.local>
2018-12-28 00:51:53 -0200

    analise de logs project part1

---
+++ index.py
@@ -0,0 +1,43 @@
+import psycopg2
+
+def popularArticles():
+    cursor.execute("select * from articles_top3") 
+    artigos = cursor.fetchall()
+    for artigo in artigos: 
+        print("%s - %s"%(artigo[0],artigo[1]))
+
+def popularAuthors():
+    cursor.execute("select * from popular_authors")
+    authors = cursor.fetchall()
+    for author in authors:
+        print("%s - %s"%(author[0],author[1]))
+
+def dayOfErrorRequest(): 
+    cursor.execute("select * from dia_maior_porc")
+    dias = cursor.fetchall()
+    for dia in dias:
+        print("%s - %s"%(dia[0],dia[1]))
+
+def main():
+    conn = psycopg2.connect("dbname=news")
+    global cursor 
+    cursor = conn.cursor()
+
+    print('-------- 3 artigos mais populares --------')
+    popularArticles()
+    print('\n')
+
+    print('-------- Autores populares --------')
+    popularAuthors()
+    print('\n')
+
+    print('-------- Mais requisicoes com erro --------')
+    dayOfErrorRequest()
+
+    conn.close()
+
+if __name__ == "__main__":
+    main()
+
+
+
